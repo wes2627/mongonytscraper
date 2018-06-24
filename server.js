f6d4bd2376a1d55143a74c21400eb7014e98fb53
Whitney Shirk <whitney.shirk@gmail.com>
2018-06-24 01:10:45 -0500

    new commit

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 
-//mongoose.connect("mongodb://heroku_jmv816f9:5j1nd4taq42hi29bfm5hobeujd@ds133192.mlab.com:33192/heroku_jmv816f9");
 
 var db = process.env.MONGOD_URI || "mongodb://localhost/mongodbnyt"
 
